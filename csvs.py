4376260b4b3150aa7c8080b29606d4cfe9840ea0
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2016-04-10 16:47:33 +0200

    new file

---
+++ csvs.py
@@ -0,0 +1,17 @@
+#
+#
+# Based on
+# http://stackoverflow.com/questions/17413261/retrieve-first-row-of-csv-using-dictionary
+#
+
+import csv
+
+
+def readintoArray(file_in):
+	resultArray = []
+	csv_file = open(file_in)
+	resultDict = csv.DictReader(csv_file)
+	for row in resultDict:
+		resultArray.append(row)
+	return resultArray
+
