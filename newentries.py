0444edbd253276b7110db5cab388c490991aac26
aaf <alonsofonseca.angel@gmail.com>
2018-06-09 08:09:27 +0200

    helper to create new entries

---
+++ newentries.py
@@ -0,0 +1,67 @@
+#/usr/bin/env python3                                                                                                                                                                                 
+"""
+Creates a list of entries to be copied to the CSV
+
+USAGE:
+    python3 newentries.py <start_date> <average_amount_you_need> <number_of_days_to_generate>
+"""
+
+import sys
+import datetime
+import time
+
+
+def generate_sequence(amount, days):
+    sequence = [1]
+    current_amount = sum(i for i in sequence) / len(sequence)
+
+    for day in range(days - 1):
+        if current_amount < amount:
+            sequence.append(2)
+        else:
+            sequence.append(1)
+        current_amount = sum(i for i in sequence) / len(sequence)
+    return sequence
+
+
+def csv_print(values, start_date):
+    if start_date == "today":
+        real_date = datetime.date.today()
+    elif start_date == "tomorrow":
+        delta = 1
+        real_date = datetime.date.today() + datetime.timedelta(days=delta)
+    else:
+        real_date = datetime.datetime.strptime(start_date, "%Y%m%d")
+    days_in = 0
+    for value in values:
+        #real_date = datetime.date.today().strftime("%Y-%m-%d %H:%M:%S")
+        current_date = real_date + datetime.timedelta(days=days_in)
+
+        testtime = str(datetime.datetime.fromtimestamp(int(time.mktime(current_date.timetuple()))).strftime("%Y-%m-%d"))
+        printable_time = testtime + "T19:00:00Z," + testtime + "T20:00:00Z" 
+        sintrom_text = "Sintrom Amount: " + str(value)
+
+        print(sintrom_text + "," + sintrom_text + "," + printable_time)
+        days_in += 1
+
+
+def show_error():
+    print("Syntax:")
+    print("  python3 newentries.py <start_date> <average_amount_you_need> <number_of_days_to_generate>")
+    print("")
+    print("  E.g.: python3 newentries.py today 1.7142857142857142 28")
+    print("  E.g.: python3 newentries.py tomorrow 1.75 56")
+    print("  E.g.: python3 newentries.py 20180614 1.75 56")
+
+
+if __name__ == '__main__':
+    try:
+        start_date = sys.argv[1]
+        average_value = float(sys.argv[2])
+        days = int(sys.argv[3])
+    except IndexError:
+        show_error()
+        sys.exit(2)
+
+    csv_print(generate_sequence(average_value, days), start_date)
+
