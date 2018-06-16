e9e5c63645e0089d78cc87302a92743940b2c581
aaf <alonsofonseca.angel@gmail.com>
2018-06-16 09:21:03 +0200

    timepicker not stable

--- AlertApp/App.js
+++ AlertApp/App.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+'use strict';                                                                                                                                                                                         
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+import {
+  StyleSheet,
+} from 'react-native';
+import {
+  StackNavigator,
+} from 'react-navigation';
+import NoConfigMsg from './NoConfigMsg';
+import GetTime from './GetTime';
+ 
+const App = StackNavigator({
+  Home: { screen: NoConfigMsg },
+  GetTime: { screen: GetTime },
 });
+export default App;
+ 
---
+++ AlertApp/GetTime.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, AsyncStorage } from 'react-native';
+ 
+import TimePicker from 'react-native-simple-time-picker';
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+ 
+export default class GetTime extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        myKey: null
+    }
+  }
+  async getKey() {
+    try {
+      const value = await AsyncStorage.getItem('@MySuperStore:key');
+      this.setState({myKey: value});
+    } catch (error) {
+      console.log("Error retrieving data" + error);
+    }
+  }
+
+  async saveKey(value) {
+    try {
+      await AsyncStorage.setItem('@MySuperStore:key', value);
+    } catch (error) {
+      console.log("Error saving data" + error);
+    }
+  }
+
+  async resetKey() {
+    try {
+      await AsyncStorage.removeItem('@MySuperStore:key');
+      const value = await AsyncStorage.getItem('@MySuperStore:key');
+      this.setState({myKey: value});
+    } catch (error) {
+      console.log("Error resetting data" + error);
+    }
+  }
+
+  state = {
+    selectedHours: 0,
+    selectedMinutes: 0,
+  }
+
+  changeTime() {
+
+  }
+ 
+  render() {
+    const { selectedHours, selectedMinutes } = this.state;
+    return (
+      <View style={styles.container}>
+        <Text>{selectedHours}:{selectedMinutes}</Text>
+        <TimePicker
+          selectedHours={selectedHours}
+          selectedMinutes={selectedMinutes}
+         // onChange={(hours, minutes) => this.setState({ selectedHours: hours, selectedMinutes: minutes })}
+          onChange={
+            (hours, minutes) => this.saveKey(minutes.toString());
+            (hours, minutes) => this.setState({ selectedHours: hours, selectedMinutes: minutes })
+          }
+        />
+      </View>
+    );
+  }
+}
